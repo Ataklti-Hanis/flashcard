8be34ee7edca90369faf68ae9099529b854f5802
AtakltiHanis <atakltihanis14@gmail.com>
2025-01-14 13:38:04 +0300

    maping the quetions

--- src/App.js
+++ src/App.js
@@ -43,22 +43,10 @@ const questions = [
 ];
 
 function FlashCards() {
-  const [selectedId, setSelectedId] = useState(null);
-  function handleClick(id) {
-    setSelectedId(id !== selectedId ? id : null);
-  }
   return (
     <div className="flashcards">
       {questions.map((question) => (
-        <div
-          key={question.id}
-          className={question.id === selectedId ? "selected" : ""}
-          onClick={() => handleClick(question.id)}
-        >
-          <p>
-            {question.id === selectedId ? question.answer : question.question}
-          </p>
-        </div>
+        <div>{question.question}</div>
       ))}
     </div>
   );
